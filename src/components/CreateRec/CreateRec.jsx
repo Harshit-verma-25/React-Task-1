bae0b5291375ec53bf3409e8bf98edc98efb42d6
Harshit-verma-25 <harshit2005verma@gmail.com>
2023-12-15 11:22:15 +0530

    Final commit

--- src/components/CreateRec/CreateRec.jsx
+++ src/components/CreateRec/CreateRec.jsx
@@ -5,12 +5,13 @@ import { db, recordCollection, storage } from '../Firebase/firebase'
 import { nanoid } from 'nanoid'
 import './CreateRec.css'
 
-function CreateRec( { id, setRecordId } ) {
+function CreateRec({ id, setRecordId }) {
     const imageRef = useRef(null)
     const [image, setImage] = React.useState('')
     const [resume, setResume] = React.useState('')
     const [addInputField, setAddInputField] = React.useState([])
     const [disable, setDisable] = React.useState(false)
+    const [editMode, SeteditMode] = React.useState(false)
     const [formData, setFormData] = React.useState({
         userID: nanoid(),
         name: '',
@@ -22,37 +23,46 @@ function CreateRec( { id, setRecordId } ) {
         dob: '',
         resume: ''
     })
-    
-    async function loadLink () {
 
-        try {    
-            let imgRef = ref(storage, `images/${formData.userID}`)
+    async function loadLink() {
 
-            let resumeRef = ref(storage, `resumes/${formData.userID}`)
+        try {
+            let imgURL, resURL
+            if (image) {
+                const imageRef = ref(storage, `images/${formData.userID}`)
+                await uploadBytes(imageRef, image);
+                imgURL = await getDownloadURL(imageRef);
+                console.log("Image URL: " + imgURL);
+            } else {
+                imgURL = formData.img;
+            }
 
-            await uploadBytes(imgRef, image)
-            const imgURL = await getDownloadURL(imgRef)
-            
-            await uploadBytes(resumeRef, resume)
-            const resURL = await getDownloadURL(resumeRef)
-            
+            if (resume) {
+                const resumeref = ref(storage, `resumes/${formData.userID}`)
+                await uploadBytes(resumeref, resume);
+                resURL = await getDownloadURL(resumeref);
+                console.log("Resume URL: " + resURL);
+            } else {
+                resURL = formData.resume;
+            }
+
             return [ imgURL, resURL ]
-        } 
+        }
         catch (error) {
             console.error('Error uploading files:', error)
         }
     }
 
-    
-    async function createNewRecord () {
-        
-        const [imgURL, resURL] = await loadLink()
+
+    async function createNewRecord() {
+
+        let [imgURL, resURL] = await loadLink()
         let data = { ...formData, qualificaion: addInputField, img: imgURL, resume: resURL }
         setFormData(data)
         setDisable(true)
 
         await setDoc(doc(db, 'record', formData.userID), data)
-        
+
         alert("Record Successfully Added")
         setFormData({
             userID: '',
@@ -70,48 +80,53 @@ function CreateRec( { id, setRecordId } ) {
         setResume('')
     }
 
-    function handleInput(event, isnumber=false) {
-        const {name, value} = event.target
-        setFormData(prevData => ({
-            ...prevData,
-            [name]: isnumber ? Number(value) : value
-        }))
+    const handleInput = (e) => {
+        let name, value;
+        if (e.target.name && e.target.value) {
+            name = e.target.name;
+            value = e.target.value;
+            setFormData({
+                ...formData,
+                [name]: value
+            })
+        }
     }
-    
-    function handleImageClick (event) {
+
+    function handleImageClick(event) {
         imageRef.current.click()
     }
-    
-    function handleImageChange (event) {
+
+    function handleImageChange(event) {
         setImage(event.target.files[0])
     }
-    
-    function handleResumeChange (event) {
+
+    function handleResumeChange(event) {
         setResume(event.target.files[0])
     }
-    
-    function handleAdd () {
+
+    function handleAdd() {
         const empty = [...addInputField, ""]
         setAddInputField(empty)
     }
 
-    function handleChange (onChangeValue, i) {
+    function handleChange(onChangeValue, i) {
         const inputData = [...addInputField]
         inputData[i] = onChangeValue.target.value
         setAddInputField(inputData)
     }
 
-    function handleDelete (i) {
+    function handleDelete(i) {
         const deleteInput = [...addInputField]
-        deleteInput.splice(i,1)
+        deleteInput.splice(i, 1)
         setAddInputField(deleteInput)
     }
 
-    async function editHandler () {
+    async function editHandler() {
         try {
             const docSnap = await getDocs(recordCollection, id)
+            SeteditMode(!editMode);
             docSnap.forEach((doc) => {
-                if (doc.id === id){
+                if (doc.id === id) {
                     const docData = doc.data()
                     setFormData(docData)
                     setAddInputField(docData.qualificaion)
@@ -123,21 +138,29 @@ function CreateRec( { id, setRecordId } ) {
         }
     }
 
-    // console.log(addInputField)
-    // console.log(image)
-    // console.log(resume)
+    async function saveEdit() {
 
-    async function saveEdit () {
-        
-        if (image === '' && resume === '') {
-            let docRef = doc(db, 'record', id)    
-            let data = { ...formData, qualificaion: addInputField}
-            setFormData(data)
-    
+        let [imgURL, resURL] = await loadLink()
+        const userToUpdate = {
+            ...formData,
+            img: imgURL,
+            resume: resURL,
+            qualificaion: addInputField
+        }
+
+        console.log(userToUpdate)
+        const hasChanges = JSON.stringify(formData) !== JSON.stringify(userToUpdate)
+        console.log(hasChanges)
+
+        if (editMode && hasChanges) {
+
+            setFormData(userToUpdate)
+            console.log(formData)
+            let docRef = doc(db, 'record', id)
             await setDoc(docRef, formData)
-            
+
             alert("Record Successfully Updated")
-    
+
             setFormData({
                 userID: '',
                 name: '',
@@ -152,54 +175,12 @@ function CreateRec( { id, setRecordId } ) {
             setAddInputField([])
         }
         else {
-            if (image == '') {
-                const [resURL] = await loadLink()
-                console.log(resURL)
-                let data = { ...formData, qualificaion: addInputField, resume: resURL }
-                console.log(data)
-                setFormData(data)
-            }
-            else if (resume == '') {
-                const [imgURL] = await loadLink()
-                console.log(imgURL)
-                let data = { ...formData, qualificaion: addInputField, img: imgURL }
-                console.log(data)
-                setFormData(data)
-            }
-            else {
-                const [imgURL, resURL] = await loadLink()
-                console.log(imgURL, "\n", resURL)
-                let data = { ...formData, qualificaion: addInputField, img: imgURL, resume: resURL }
-                console.log(data)
-                setFormData(data)
-            }       
-
-
-            console.log(formData)
-            let docRef = doc(db, 'record', id)
-            await setDoc(docRef, formData)
-            
-            alert("Record Successfully Updated")
-    
-            setFormData({
-                userID: '',
-                name: '',
-                img: '',
-                address: '',
-                contact: '',
-                email: '',
-                qualificaion: [],
-                dob: '',
-                resume: ''
-            })
-            setAddInputField([])
-            setImage('')
-            setResume('')
+            alert("No Changes has been made")
         }
     }
 
     React.useEffect(() => {
-        if (id !== undefined && id !== ""){
+        if (id !== undefined && id !== "") {
             editHandler()
         }
     }, [id])
@@ -209,29 +190,29 @@ function CreateRec( { id, setRecordId } ) {
             <div className='form'>
                 <div className='form-container'>
                     <div className="form-input">
-                        Name: <input type="text" name='name' className="input" placeholder="Enter your Name" 
-                        value={formData.name} onChange={handleInput} autoComplete='true' required/>
+                        Name: <input type="text" name='name' className="input" placeholder="Enter your Name"
+                            value={formData.name} onChange={handleInput} autoComplete='true' required />
 
-                        Address: <input type="text" name='address' className="input" placeholder="Enter your Address" 
-                        value={formData.address} onChange={handleInput} autoComplete='true' required/>
+                        Address: <input type="text" name='address' className="input" placeholder="Enter your Address"
+                            value={formData.address} onChange={handleInput} autoComplete='true' required />
 
-                        Contact: <input type="text" name='contact' className="input" placeholder="Enter your Contact" 
-                        value={formData.contact} onChange={(e) => handleInput(e,true)} autoComplete='true' required/>
-                        
-                        Email: <input type="text" name='email' className="input" placeholder="Enter your Email" 
-                        value={formData.email} onChange={handleInput} autoComplete='true' required/>
-                        
-                        DOB: <input type="date" name='dob' className="input" placeholder="Enter your DOB" 
-                        value={formData.dob} onChange={handleInput} autoComplete='true' required/>
+                        Contact: <input type="text" name='contact' className="input" placeholder="Enter your Contact"
+                            value={formData.contact} onChange={(e) => handleInput(e, true)} autoComplete='true' required />
+
+                        Email: <input type="text" name='email' className="input" placeholder="Enter your Email"
+                            value={formData.email} onChange={handleInput} autoComplete='true' required />
+
+                        DOB: <input type="date" name='dob' className="input" placeholder="Enter your DOB"
+                            value={formData.dob} onChange={handleInput} autoComplete='true' required />
                     </div>
                     <div className='profile' onClick={handleImageClick}>
-                        <img className='image-preview' src={image != '' ? URL.createObjectURL(image) : '/image-upload.png' } alt="" />
-                        
-                        <input type="file" className='input' 
-                        ref={imageRef} onChange={handleImageChange} required style={{display: "none"}}/>
+                        <img className='image-preview' src={image != '' ? URL.createObjectURL(image) : '/image-upload.png'} alt="" />
 
-                        <input type="file" name="img" className='input' onChange={handleInput} required 
-                        style={{display:'none'}}/>
+                        <input type="file" className='input'
+                            ref={imageRef} onChange={handleImageChange} required style={{ display: "none" }} />
+
+                        <input type="file" name="img" className='input' onChange={handleInput} required
+                            style={{ display: 'none' }} />
                     </div>
                 </div>
                 <div className='other-input'>
@@ -240,19 +221,19 @@ function CreateRec( { id, setRecordId } ) {
                     </div>
                     {addInputField.map((data, i) => {
                         return (
-                                <div className='add-input' key={i}>    
-                                    <input type='text' value={data} placeholder='Enter your Qualification' onChange={e => handleChange(e,i)} />
-                                    <button onClick={() => handleDelete(i)}>-</button>
-                                </div>
+                            <div className='add-input' key={i}>
+                                <input type='text' value={data} placeholder='Enter your Qualification' onChange={e => handleChange(e, i)} />
+                                <button onClick={() => handleDelete(i)}>-</button>
+                            </div>
                         )
                     })}
                 </div>
                 Resume (PDF only): <input type="file" className='input' onChange={handleResumeChange} />
-                <input type="file" name="resume" className='input' onChange={handleInput} style={{display: 'none'}} />
+                <input type="file" name="resume" className='input' onChange={handleInput} style={{ display: 'none' }} />
                 <div className='form-button'>
                     <button type="submit" className='button' onClick={createNewRecord} disabled={disable}>SAVE</button>
                     <a href={'/'}><button className='button'>BACK</button></a>
-                </div>  
+                </div>
             </div>
         )
     }
@@ -261,29 +242,29 @@ function CreateRec( { id, setRecordId } ) {
             <div className='form'>
                 <div className='form-container'>
                     <div className="form-input">
-                        Name: <input type="text" name='name' className="input" placeholder="Enter your Name" 
-                        value={formData.name} onChange={handleInput} autoComplete='true' required/>
-        
-                        Address: <input type="text" name='address' className="input" placeholder="Enter your Address" 
-                        value={formData.address} onChange={handleInput} autoComplete='true' required/>
-        
-                        Contact: <input type="text" name='contact' className="input" placeholder="0" 
-                        value={formData.contact} onChange={(e) => handleInput(e,true)} autoComplete='true' required/>
-                        
-                        Email: <input type="text" name='email' className="input" placeholder="Enter your Email" 
-                        value={formData.email} onChange={handleInput} autoComplete='true' required/>
-                        
-                        DOB: <input type="date" name='dob' className="input" placeholder="Enter your DOB" 
-                        value={formData.dob} onChange={handleInput} autoComplete='true' required/>
+                        Name: <input type="text" name='name' className="input" placeholder="Enter your Name"
+                            value={formData.name} onChange={handleInput} autoComplete='true' required />
+
+                        Address: <input type="text" name='address' className="input" placeholder="Enter your Address"
+                            value={formData.address} onChange={handleInput} autoComplete='true' required />
+
+                        Contact: <input type="text" name='contact' className="input" placeholder="0"
+                            value={formData.contact} onChange={(e) => handleInput(e, true)} autoComplete='true' required />
+
+                        Email: <input type="text" name='email' className="input" placeholder="Enter your Email"
+                            value={formData.email} onChange={handleInput} autoComplete='true' required />
+
+                        DOB: <input type="date" name='dob' className="input" placeholder="Enter your DOB"
+                            value={formData.dob} onChange={handleInput} autoComplete='true' required />
                     </div>
                     <div className='profile' onClick={handleImageClick}>
                         <img className='image-preview' src={image == '' ? formData.img : URL.createObjectURL(image)} alt="" />
-                        
-                        <input type="file" className='input' 
-                        ref={imageRef} onChange={handleImageChange} required style={{display: "none"}}/>
 
-                        <input type="file" name="img" className='input' onChange={handleInput} required 
-                        style={{display:'none'}}/>
+                        <input type="file" className='input'
+                            ref={imageRef} onChange={handleImageChange} required style={{ display: "none" }} />
+
+                        <input type="file" name="img" className='input' onChange={handleInput} required
+                            style={{ display: 'none' }} />
                     </div>
                 </div>
                 <div className='other-input'>
@@ -292,18 +273,18 @@ function CreateRec( { id, setRecordId } ) {
                     </div>
                     {addInputField.map((data, i) => {
                         return (
-                            <div className='add-input' key={i}>    
-                                <input type='text' value={data} placeholder='Enter your Qualification' onChange={e => handleChange(e,i)} />
+                            <div className='add-input' key={i}>
+                                <input type='text' value={data} placeholder='Enter your Qualification' onChange={e => handleChange(e, i)} />
                                 <button onClick={() => handleDelete(i)}>-</button>
                             </div>
                         )
                     })}
                 </div>
-                Resume (PDF only): <input type="file" name="resume" className='input' onChange={handleResumeChange}/>
+                Resume (PDF only): <input type="file" name="resume" className='input' onChange={handleResumeChange} />
                 <div className='form-button'>
                     <button type="submit" className='button' onClick={saveEdit}>EDIT</button>
                     <a href={'/view-record'}><button className='button'>CANCEL</button></a>
-                </div>  
+                </div>
             </div>
         )
     }
--- src/components/CreateRec/viewRec.jsx
+++ src/components/CreateRec/viewRec.jsx
@@ -1,3 +1,94 @@
+// import React, { useState, useEffect } from "react"
+// import { doc, getDocs, deleteDoc } from "firebase/firestore"
+// import { ref, deleteObject, listAll, list } from 'firebase/storage'
+// import { recordCollection, storage } from "../Firebase/firebase"
+// import { Link } from "react-router-dom"
+// import './viewRec.css'
+
+// function ViewRec ( { id, getRecordId } ) {
+//     const [viewData, setViewData] = React.useState([])
+
+//     React.useEffect(() => {
+//         readData();
+//     }, [])
+
+//     async function readData () {
+//         const querySnapshot = await getDocs(recordCollection)
+    
+//         const dataArray = []
+//         querySnapshot.forEach((doc) => {
+//             const data = doc.data()
+//             dataArray.push({id: doc.id, ...data})
+//             setViewData(dataArray)
+//         })
+//     }
+
+//     async function handleDelete (id, imgURL, resURL) {
+
+//         console.log(id)
+        
+//         const deleteImgRef = ref(storage, `images/${id}`)
+//         await deleteObject(deleteImgRef)
+        
+//         const deleteResRef = ref(storage, `resumes/${id}`)
+//         await deleteObject(deleteResRef)
+//         console.log("Data Deleted")
+        
+//         setViewData((prevData) => prevData.filter((data) => data.userID !== id))
+//         await deleteDoc(doc(recordCollection, id))
+        
+//         readData()
+//     }
+
+//     return (
+//         <>
+//             <h1>View Record</h1>
+//             <br />
+//             <table>
+//                 <thead>
+//                     <tr>
+//                         <th>S.No</th>
+//                         <th>Name</th>
+//                         <th>Email</th>
+//                         <th>Contact</th>
+//                         <th>Profile</th>
+//                         <th>Resume</th>
+//                         <th colSpan={2}>Action</th>
+//                     </tr>
+//                 </thead>
+//                 <tbody>
+//                     {
+//                         viewData.map((item, index) => (
+//                             <tr key={item.userID}>
+//                                 <td>{index + 1}</td>
+//                                 <td>{item.name}</td>
+//                                 <td>{item.email}</td>
+//                                 <td>{item.contact}</td>
+//                                 <td><img className="image-pre" src={item.img} alt="" /></td>
+//                                 <td>
+//                                     <Link to={`${item.resume}`} target="_blank">
+//                                         <button>View</button>
+//                                     </Link>
+//                                 </td>
+//                                 <td>
+//                                     <Link to={'/create-record'}><button onClick={(e) => getRecordId(item.userID)}>Edit</button></Link>
+//                                     <button onClick={() => handleDelete(item.id, item.img, item.resume)}>Delete</button>
+//                                 </td>
+//                             </tr>
+//                         ))
+//                     }
+//                 </tbody>
+//             </table>
+//             <Link to={'/'}><button>Back</button></Link>
+//         </>   
+//     )
+// }
+
+// export default ViewRec
+
+
+
+
 import React, { useState, useEffect } from "react"
 import { doc, getDocs, deleteDoc } from "firebase/firestore"
 import { ref, deleteObject, listAll, list } from 'firebase/storage'
@@ -5,38 +96,41 @@ import { recordCollection, storage } from "../Firebase/firebase"
 import { Link } from "react-router-dom"
 import './viewRec.css'
 
-function ViewRec ( { id, getRecordId } ) {
+function ViewRec({ id, getRecordId }) {
     const [viewData, setViewData] = React.useState([])
 
     React.useEffect(() => {
         readData();
     }, [])
 
-    async function readData () {
-        const querySnapshot = await getDocs(recordCollection)
-    
-        const dataArray = []
-        querySnapshot.forEach((doc) => {
-            const data = doc.data()
-            dataArray.push({id: doc.id, ...data})
-            setViewData(dataArray)
-        })
+    async function readData() {
+        try {
+            const querySnapshot = await getDocs(recordCollection);
+            const dataArray = [];
+            querySnapshot.forEach((doc) => {
+                const data = doc.data();
+                dataArray.push({ id: doc.id, ...data });
+            });
+            setViewData(dataArray);
+        } catch (error) {
+            console.error("Error reading data:", error);
+        }
     }
 
-    async function handleDelete (id, imgURL, resURL) {
+    async function handleDelete(id, imgURL, resURL) {
 
         console.log(id)
-        
+
         const deleteImgRef = ref(storage, `images/${id}`)
         await deleteObject(deleteImgRef)
-        
+
         const deleteResRef = ref(storage, `resumes/${id}`)
         await deleteObject(deleteResRef)
         console.log("Data Deleted")
-        
+
         setViewData((prevData) => prevData.filter((data) => data.userID !== id))
         await deleteDoc(doc(recordCollection, id))
-        
+
         readData()
     }
 
@@ -80,7 +174,7 @@ function ViewRec ( { id, getRecordId } ) {
                 </tbody>
             </table>
             <Link to={'/'}><button>Back</button></Link>
-        </>   
+        </>
     )
 }
 
